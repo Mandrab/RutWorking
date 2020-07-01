1fb22de3e118d1bda17653c6503f589a9b451561
Paolo Baldini <paolobaldini01@gmail.com>
2020-07-01 17:16:12 +0200

    added test

--- src/test/routes/module.ts
+++ src/test/routes/module.ts
@@ -69,14 +69,34 @@ describe('test module creation', function() {
 
         await new DBProject({ name: CHIEF.project.name, chief: chief._id(), modules: [] }).save()
 
-        // valid token
+        // no token
         request.post('/projects/' + CHIEF.project.name + '/modules/' + CHIEF.project.modules[0]).expect(500)
             .expect('Token has not been passed!').end((err: any) => {
             if (err) { console.log(err); return Promise.reject() }
         })
 
+        // user token (not chief)
+        request.post('/projects/' + CHIEF.project.name + '/modules/' + CHIEF.project.modules[0])
+            .set({ 'Authorization': userToken }).expect(403).end((err: any) => {
+            if (err) { console.log(err); return Promise.reject() }
+        })
+
+        // no project
+        request.post('/projects/FAKE' + CHIEF.project.name + '/modules/' + CHIEF.project.modules[0])
+            .set({ 'Authorization': chiefToken }).expect(404).end((err: any) => {
+            if (err) { console.log(err); return Promise.reject() }
+        })
+
         // valid token
         request.post('/projects/' + CHIEF.project.name + '/modules/' + CHIEF.project.modules[0])
+            .set({ 'Authorization': chiefToken }).expect(201).end((err: any) => {
+            if (err) { console.log(err); return Promise.reject() }
+        })
+
+        // TODO: OK? -> NO ERROR TRYING TO ADD A NEW MODULE WITH EXISTING NAME
+
+        // valid token
+        request.post('/projects/' + CHIEF.project.name + '/modules/' + CHIEF.project.modules[1])
             .set({ 'Authorization': chiefToken }).expect(201).end((err: any) => {
             if (err) { console.log(err); return Promise.reject() }
         })
