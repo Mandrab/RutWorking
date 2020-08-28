b1911cb90aa8c9025cb79664d72e8d0bb3fdeb0d
Paolo Baldini <paolobaldini01@gmail.com>
2020-08-28 13:26:45 +0200

    added some docs

--- server/src/main/config/firebase.ts
+++ server/src/main/config/firebase.ts
@@ -1,8 +1,13 @@
+/**
+ * Configure rutworking service to work with firebase
+ * 
+ * @author Paolo Baldini
+ */
 import admin from 'firebase-admin'
 import keyData from './firebase_key.json'
 
 admin.initializeApp({
     credential: admin.credential.cert(keyData as any),
-    databaseURL: 'https://rutworking-fb3b3.firebaseio.com'
+    databaseURL: 'https://rutworking-fb3b3.firebaseio.com'  // TODO needed?
 })
 export let _admin = admin
