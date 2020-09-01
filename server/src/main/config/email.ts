1d9a91de7d80ce4391a9010bc582f05f9633b9e4
Marco Meluzzi <marco.meluzzi@studio.unibo.it>
2020-09-01 23:22:53 +0200

    Reset old configuration file (erroneously pushed)

---
+++ server/src/main/config/email.ts
@@ -0,0 +1,10 @@
+/**
+ * Contains config params for email send
+ * 
+ * @author Paolo Baldini
+ */
+export const config = {
+    service: 'your-service',
+    email: 'your-email',
+    password: 'password'
+}
